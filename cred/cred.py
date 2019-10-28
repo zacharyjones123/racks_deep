e407392970e8c343ce2d308f85e0f1706b323556
Zachary Jones <zrjones77@gmail.com>
2019-10-28 16:50:11 -0400

    asdfasdf

--- cred/cred.py
+++ cred/cred.py
@@ -5,3 +5,4 @@ API_KEY = "a8318ba6ec81130322497e0e213ad1ae"
 PASSWORD = "0171e35f0140499ac3b8a19a4ad166d4"
 SHARED_SECRET = "3472f87f94cd07d94971db3222da1b87"
 SHOP_URL = "https://%s:%s@racks-deep-custom-performance.myshopify.com/admin" % (API_KEY, PASSWORD)
+asdfasdf
